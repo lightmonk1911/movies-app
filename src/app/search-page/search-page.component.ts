cb3d4c4a32533a6f6e47922531f2f7f020302545
igor_babenkov <ibabenkov@htc-cs.ru>
2019-04-29 17:39:45 +0400

    fixed complexity

--- src/app/search-page/search-page.component.ts
+++ src/app/search-page/search-page.component.ts
@@ -15,6 +15,47 @@ import {
 import { Button } from '../button/button.model';
 import { selectFavoriteMovies, selectWatchedMovies, selectSearchResult } from '../reducers';
 
+const searchProjector = (
+  favoriteList: Array<IMovie>,
+  searchResult: ISearchResult,
+  watchedList: Array<IMovie>
+) => {
+  if (searchResult.error) {
+    return [];
+  }
+  return searchResult.movies.map((movie) => {
+    const watched = watchedList.find(
+      (watchedMovie: IMovie) => watchedMovie.imdbID === movie.imdbID
+    );
+    const favorite = favoriteList.find(
+      (favoriteMovie: IMovie) => favoriteMovie.imdbID === movie.imdbID
+    );
+    const watchedBtn = new Button(watched ? inWatched : addToWatchedBtn, () =>
+      this.store.dispatch({
+        type: watched ? inWatched.actionType : addToWatchedBtn.actionType,
+        payload: { movie }
+      })
+    );
+    const favoriteBtn = new Button(favorite ? inFavorite : addToFavoriteBtn, () =>
+      this.store.dispatch({
+        type: favorite ? inFavorite.actionType : addToFavoriteBtn.actionType,
+        payload: { movie }
+      })
+    );
+    return {
+      movie,
+      buttons: [favoriteBtn, watchedBtn]
+    };
+  });
+};
+
+const combinedSelector = createSelector(
+  selectFavoriteMovies,
+  selectSearchResult,
+  selectWatchedMovies,
+  searchProjector
+);
+
 @Component({
   selector: 'app-search-page',
   templateUrl: './search-page.component.html',
@@ -65,41 +106,7 @@ export class SearchPageComponent implements OnInit {
   }
 
   ngOnInit() {
-    const foundMoviesSelector = createSelector(
-      selectFavoriteMovies,
-      selectSearchResult,
-      selectWatchedMovies,
-      (favoriteList: Array<IMovie>, searchResult: ISearchResult, watchedList: Array<IMovie>) => {
-        if (searchResult.error) {
-          return [];
-        }
-        return searchResult.movies.map((movie) => {
-          const watched = watchedList.find(
-            (watchedMovie: IMovie) => watchedMovie.imdbID === movie.imdbID
-          );
-          const favorite = favoriteList.find(
-            (favoriteMovie: IMovie) => favoriteMovie.imdbID === movie.imdbID
-          );
-          const watchedBtn = new Button(watched ? inWatched : addToWatchedBtn, () =>
-            this.store.dispatch({
-              type: watched ? inWatched.actionType : addToWatchedBtn.actionType,
-              payload: { movie }
-            })
-          );
-          const favoriteBtn = new Button(favorite ? inFavorite : addToFavoriteBtn, () =>
-            this.store.dispatch({
-              type: favorite ? inFavorite.actionType : addToFavoriteBtn.actionType,
-              payload: { movie }
-            })
-          );
-          return {
-            movie,
-            buttons: [favoriteBtn, watchedBtn]
-          };
-        });
-      }
-    );
-    this.moviesToShowPrepared$ = this.store.select(foundMoviesSelector);
+    this.moviesToShowPrepared$ = this.store.select(combinedSelector);
     this.countOfMovies$ = this.store.select(({ searchResult: { totalMovies } }) => totalMovies);
     this.searchResult$ = this.store.select('searchResult');
     this.store
